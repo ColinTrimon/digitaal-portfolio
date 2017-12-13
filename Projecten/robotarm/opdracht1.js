addaec64e95b1dedfd40b4a1faa46793bc3e7a74
Colin de Wit <99034809@mydavinci.nl>
2017-12-13 09:43:17 +0100

    opdracht ingeleverd

---
+++ Projecten/robotarm/opdracht1.js
@@ -0,0 +1,10 @@
+<script>
+  var canvas = document.getElementById("canvas");
+  var robotArm = new RobotArm(canvas);
+
+  robotArm.loadLevel("exercise 1");
+
+  // Je eigen code plaats je hier.
+
+  robotArm.run();
+</script>
